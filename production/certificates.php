5520a6978e95985a0941c483b78d0052d2257462
Joeri Kassenaar <joeri@Joeris-MacBook-Air.local>
2024-12-07 17:28:37 +0100

    Fix server names on production

--- production/certificates.php
+++ production/certificates.php
@@ -1,15 +1,14 @@
 <?php
 
-// echo chr(27) . chr(91) . 'H' . chr(27) . chr(91) . 'J';
 echo "\e[H\e[J \n";
 
 echo "Copy letsencrypt keys to apache folders. \n\n";
 
-$cmd = "sudo cp /etc/letsencrypt/live/bespired.com/fullchain.pem ~/eventlab/event-lab-server/docker/apache/mycert.crt";
+$cmd = "sudo cp /etc/letsencrypt/live/bespired.com/fullchain.pem ~/eventlab/event-lab-server/docker/apache/ssl/mycert.crt";
 echo "$cmd \n";
 print_r(shell_exec($cmd));
 
-$cmd = "sudo cp /etc/letsencrypt/live/bespired.com/privkey.pem   ~/eventlab/event-lab-server/docker/apache/mycert.key";
+$cmd = "sudo cp /etc/letsencrypt/live/bespired.com/privkey.pem   ~/eventlab/event-lab-server/docker/apache/ssl/mycert.key";
 echo "$cmd \n";
 print_r(shell_exec($cmd));
 
--- production/servername.php
+++ production/servername.php
@@ -1,13 +1,25 @@
 <?php
 
-$servername = "bespired.com";
-
+$servername = "bespired";
 // change `eventlab.com` to current servername
 
-$filenames = ["docker-compose.yml"];
+$filenames = [
+    "docker-compose.yml",
+    "profile_html/docker.env",
+    "tracker_html/docker.env",
+    "public_html/docker.env",
+    "public_html/public/.htaccess",
+    "profile_html/public/.htaccess",
+    "profile_html/database/seeds/projects/projects.yaml",
+];
 
 foreach ($filenames as $filename) {
-    $content = file_get_contents($filename);
-    $content = str_replace('eventlab.com', $servername, $content);
-    file_put_contents($filename, $content);
+    if (file_exists($filename)) {
+        $content = file_get_contents($filename);
+        $content = str_replace('eventlab', $servername, $content);
+        file_put_contents($filename, $content);
+        echo "Swapped eventlab into $servername in file $filename \n";
+    } else {
+        echo "Cannot find $filename \n";
+    }
 }
